1273a37b3e8894943eb1a1596737a90eb24b1210
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-08-15 00:50:16 +0530

    total surface area of cylinder program added

---
+++ Java/BasicJavaPrograms/TotalSurfaceAreaOfCylinder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class TotalSurfaceAreaOfCylinder {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Total Surface Area of a cylinder :");
+
+        System.out.print("Enter the radius of the cylinder : ");
+        double radius=sc.nextDouble();
+        System.out.print("Enter the height of the cylinder : ");
+        double height=sc.nextDouble();
+
+        double TSA=TotalSurfaceAreaCylinder(radius,height);
+
+        System.out.println("Total Surface Area of the cylinder with radius = "+radius+"cm and height = "+height+"cm = "+TSA+" cm^3");
+    }
+    public static double TotalSurfaceAreaCylinder(double rad, double hgt) {
+        double volume=2*(Math.PI*rad*hgt);
+
+        return volume;
+    }
+}
