431e2b9461dae4d48692617e4f84088ba6b59d16
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-08-16 01:41:03 +0530

    Update and rename TotalSurfaceAreaOfCylinder.java to CurvedSurfaceAreaOfCylinder.java

--- Java/BasicJavaPrograms/CurvedSurfaceAreaOfCylinder.java
+++ Java/BasicJavaPrograms/CurvedSurfaceAreaOfCylinder.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class TotalSurfaceAreaOfCylinder {
+public class CurvedSurfaceAreaOfCylinder {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
