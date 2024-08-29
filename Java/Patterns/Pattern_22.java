2a08b2e440af4ea3afa48c52097735fc1a184129
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-08-29 11:13:44 +0530

    Pattern 22 with solution added
           1
           0 1
           1 0 1
           0 1 0 1
           1 0 1 0 1

---
+++ Java/Patterns/Pattern_22.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No of Rows : ");
+        int num = sc.nextInt();
+
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row; col++) {
+                if ((row % 2 != 0 && col % 2 != 0) || (row % 2 == 0 && col % 2 == 0))
+                    System.out.print("1 ");
+                else
+                    System.out.print("0 ");
+
+            }
+            System.out.println();
+        }
+    }
+}
