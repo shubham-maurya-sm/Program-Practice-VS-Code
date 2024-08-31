a57749150bdbfe6e964683c427f42a7bb6fd52ca
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-08-31 13:33:42 +0530

    pattern 7 with solution added

---
+++ Java/Patterns/Pattern_07.java
@@ -0,0 +1,31 @@
+/*
+        * * * * * 
+          * * * *
+            * * *
+              * *
+                *
+*/
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_07 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+       System.out.print("Enter Number of Rows : ");
+       int num=sc.nextInt();
+
+
+        for (int row = num; row >=0; row--) {
+            for (int col = num; col > 0; col--) {
+                if(row>=col)
+                System.out.print("* ");
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+           }
+    }
+}
