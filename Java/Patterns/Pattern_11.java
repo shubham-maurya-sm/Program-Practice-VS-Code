f731c32158255c1648afa1011ad31ce12bf5c966
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-08-31 13:38:15 +0530

    pattern 11 with solution added

---
+++ Java/Patterns/Pattern_11.java
@@ -0,0 +1,30 @@
+/*
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *
+*/
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_11 {
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+
+       System.out.print("Enter Number of Rows : ");
+       int num=sc.nextInt();
+
+       for (int row = num; row >=0; row--) {
+        for (int col = 1; col <= num; col++) {
+            if(row>=col)
+            System.out.print(" ");
+            else
+            System.out.print("* ");
+        }
+        System.out.println();
+       }
+    }
+}
